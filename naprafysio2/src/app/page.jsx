830514a4e8811824eccc26fa9afbe8e1eb8d67e3
AnnaKrifors <annsisen1@gmail.com>
2024-10-28 11:26:33 +0100

    tried portabletext

--- naprafysio2/src/app/page.jsx
+++ naprafysio2/src/app/page.jsx
@@ -1,20 +1,19 @@
 import Heading from "./components/Heading";
 import Image from "next/image";
+import { PortableText } from "@portabletext/react";
 import { getIndexPageData } from "../../utils/sanityClient.mjs";
 
 export default async function HomePage({ data }) {
   let sanityData = await getIndexPageData();
   console.log("data från sanity:", sanityData);
 
+  console.log("Sanity content data:", JSON.stringify(sanityData.content, null, 2));
+
   return (
     <div>
       <Image layout="responsive" src="/images/testimg.jpg" alt="Beskrivande text för bilden" width={1600} height={900} />
-      <Heading>NAPRAFYSIO</Heading>
-      <p>{sanityData.title}</p>
-      <p className="font-open-sans">
-        detta är alltså sidans startsida?? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas voluptatem expedita reprehenderit asperiores
-        dicta odio, sapiente voluptatibus laborum aliquid corrupti molestias, exercitationem rem iure culpa totam. Animi nostrum eveniet hic.
-      </p>
+      <Heading>{sanityData.title}</Heading>
+      <PortableText value={sanityData.content} />
     </div>
   );
 }
